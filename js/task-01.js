1e0de5db65dce61724c47516bf7485401fbc8ab9
Yuriy Brovko <ysb.ptc@gmail.com>
2022-04-19 18:39:51 +0300

    Update task-01.js

---
+++ js/task-01.js
@@ -0,0 +1,15 @@
+
+const categories = document.querySelectorAll('.item');
+
+const NumberOfCategories = categories.length;
+console.log(`Number of categories: ${NumberOfCategories}`);
+
+for (let category of categories) {
+    console.log(`Category: ${category.firstElementChild.textContent}`);
+    console.log(`Elements: ${category.lastElementChild.children.length}`) 
+}
+
+
+
+
+
