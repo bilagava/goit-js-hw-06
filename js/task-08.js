f6c0f166f505e443aef2e0964a838caef27901fd
Yuriy Brovko <ysb.ptc@gmail.com>
2022-04-26 11:25:05 +0300

    js\task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,21 @@
 
+const form = document.querySelector(".login-form");
+
+form.addEventListener('submit', onFormSubmit)
+
+function onFormSubmit(event) {
+    event.preventDefault()
+    const formData = new FormData(event.currentTarget)
+   
+    formData.forEach((value, name) => {
+        if (value === '') {
+            alert('Все поля должны быть заполнены');
+            return
+        } 
+        console.log(name, value);
+        event.currentTarget.reset();
+    });   
+}
+
+
+
