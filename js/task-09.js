230dc9640a18efbb19d62298bc8b734b3e56d76f
Yuriy Brovko <ysb.ptc@gmail.com>
2022-04-26 15:30:41 +0300

    js\task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
+const button = document.querySelector('.change-color')
+
+const backgroundColor = document.querySelector('.color')
+
+const bodyEl = document.querySelector("body");
+
+button.addEventListener('click', getRandomHexColor)
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  const newBackgroundColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`
+  backgroundColor.textContent = newBackgroundColor;
+  bodyEl.style.backgroundColor = newBackgroundColor;
 }
+
+
