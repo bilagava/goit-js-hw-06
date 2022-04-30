12d5b449ebfeede0f67cfc0fc8857745189c3c54
Yuriy Brovko <ysb.ptc@gmail.com>
2022-04-30 17:27:14 +0300

    з доработками

--- js/task-08.js
+++ js/task-08.js
@@ -6,14 +6,17 @@ form.addEventListener('submit', onFormSubmit)
 function onFormSubmit(event) {
     event.preventDefault()
     const formData = new FormData(event.currentTarget)
+    const userEmail = event.currentTarget.elements.email.value;
+    const userPass = event.currentTarget.elements.password.value;
    
     formData.forEach((value, name) => {
-        if (value === '') {
+        
+        if (userEmail === '' || userPass === '') {
             alert('Все поля должны быть заполнены');
             return
         } 
-        console.log(name, value);
-        event.currentTarget.reset();
+        console.log({ name, value });
+        event.currentTarget.reset();  
     });   
 }
 
